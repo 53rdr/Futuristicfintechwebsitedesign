ea84a8cb68c9d9ec5622b066588dbbf76c361596
53rdr <zeeshansardarofficial@gmail.com>
2026-03-24 06:23:28 -0700

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -99,7 +99,7 @@ export function Hero() {
           <div className="relative" style={{ paddingBottom: '56.25%', height: 0 }}>
             <iframe 
               className="absolute top-0 left-0 w-full h-full rounded-xl"
-              src="https://www.youtube.com/embed/YOUR_VIDEO_ID" 
+              src="https://youtu.be/it-yc41Llmw" 
               title="How Rebatrix Works"
               frameBorder="0" 
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
